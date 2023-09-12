e077e6f9ff01992e6f3821a2d2f5ba78430f9406
alexvcs <alexcurtisslep@gmail.com>
2023-09-12 18:23:53 -0400

    setting up the project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import duguetia_phaeoclados from '$lib/assets/duguetia_phaeoclados.jpg';
+</script>
+
+<h1 class="text-center text-xl mt-4 mb-2">What is the Duguetia genus?</h1>
+<figure class="text-center pb-4">
+    <img class="mx-auto" src={duguetia_phaeoclados} alt="duguetia phaeoclados plant">
+    <figcaption>Two year old Duguetia phaeoclados plant</figcaption>
+</figure>
+<div class="flex justify-center	text-center">
+    <p>
+        According to <a href="https://en.wikipedia.org/wiki/Duguetia">Wikipedia</a>,
+        Duguetia is a genus of trees and shrubs in the plant family Annonaceae with approximately
+        90 species in central and South America, and four species in west Africa.
+    </p>
+</div>
